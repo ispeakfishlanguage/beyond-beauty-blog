983e2127f52d5b97672d80617826a758c4e6d46b
ispeakfishlang <sandleguiz@gmail.com>
2024-01-10 13:58:04 +0100

    Add Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Comment, Post
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ("status", "date_posted", "category")
+    list_display = ("title", "slug", "author", "date_posted", "status", "category")
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ("author", "content", "post", "date_posted", "updated_on", "approved")
+    list_filter = ("approved", "date_posted", "updated_on")
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 
 STATUS = ((0, "Draft"), (1, "Publish"))
@@ -22,6 +23,19 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
