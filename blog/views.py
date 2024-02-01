4d29b6ff9cad7191a626570ef37f6a9816f09cab
ispeakfishlang <sandleguiz@gmail.com>
2024-02-01 23:51:27 +0100

    Add 'PostList' view on views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-date_posted')
+    template_name = 'index.html'
+    paginate_by = 10
