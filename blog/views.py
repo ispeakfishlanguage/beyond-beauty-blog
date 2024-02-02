135480d419d6ab29f5b643c8a13d246f933501e9
ispeakfishlang <sandleguiz@gmail.com>
2024-02-02 01:42:02 +0100

    Add functionality to view a post with its likes and comments

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.views import generic, View
 from .models import Post
 
 
@@ -9,3 +9,24 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-date_posted')
     template_name = 'index.html'
     paginate_by = 10
+
+
+class PostDetail(View):
+
+    def get(self, request, slug):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-date_posted')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'liked': liked,
+            }
+        )
