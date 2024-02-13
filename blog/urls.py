d2b9efcde856c30e131d43ac66225bac1367487c
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-02-13 22:10:35 +0100

    Update urls.py PEP8 Format

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,24 @@ urlpatterns = [
     path('mydrafts/', views.UserDraftsList.as_view(), name='user_drafts'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
-    path('comment/<int:comment_id>/edit/', views.CommentUpdate.as_view(), name='edit_comment'),
-    path('comment/<int:comment_id>/delete/', views.CommentDelete.as_view(), name='delete_comment'),
+    path(
+        'comment/<int:comment_id>/edit/',
+        views.CommentUpdate.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        'comment/<int:comment_id>/delete/',
+        views.CommentDelete.as_view(),
+        name='delete_comment'),
     path('post/create/', views.PostCreate.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', views.PostDelete.as_view(), name='post-delete'),
+    path(
+        'post/<int:pk>/update/',
+        views.PostUpdate.as_view(),
+        name='post-update'
+    ),
+    path(
+        'post/<int:pk>/delete/',
+        views.PostDelete.as_view(),
+        name='post-delete'
+    ),
 ]
